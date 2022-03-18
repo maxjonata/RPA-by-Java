1cdbb5a3ff5bf593356b3a0a1612a5b3fd4b4306
JAMaxwell <max.kawaii@hotmail.com>
2022-03-17 22:54:59 -0300

    test: Some Adapter tests

--- src/test/java/com/maxjonata/AppTest.java
+++ src/test/java/com/maxjonata/AppTest.java
@@ -1,8 +1,13 @@
 package com.maxjonata;
 
-import static org.junit.Assert.assertTrue;
-
+import com.maxjonata.adapters.CreateChromeDriver;
+import com.maxjonata.adapters.MakeAdapter;
+import com.maxjonata.controllers.LoggerDisabled;
+import com.maxjonata.controllers.LoggerSout;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -16,5 +21,23 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithDefaultDisabledLogger()
+    {
+        assertTrue(new MakeAdapter().logger(null) instanceof LoggerDisabled);
+    }
+    
+    @Test
+    public void shouldAnswerWithSelectedLogger()
+    {
+        assertTrue(new MakeAdapter().logger(new LoggerSout()) instanceof LoggerSout);
+    }
+
+    @Test
+    public void shouldAnswerWithANewChromeDriver()
+    {
+        assertTrue(CreateChromeDriver.make() instanceof ChromeDriver);
     }
 }
